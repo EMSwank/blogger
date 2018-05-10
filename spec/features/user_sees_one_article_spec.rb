8ebcc4783a879fb1a935ebf7f417397234c98e13
Eliot Swank <eliotswank@gmail.com>
2018-05-10 15:28:22 -0600

    adds 'Back to Articles List' funtionality on article show page

--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 describe "user sees one article" do
   describe "they link from the articles index" do
     it "displays information for one article" do
-      article = Article.create(title: "New Title", body: "New Body")
+      article = Article.create!(title: "New Title", body: "New Body")
 
       visit articles_path
 
       click_link article.title
-
+save_and_open_page
       expect(page).to have_content(article.title)
       expect(page).to have_content(article.body)
     end
